7264b41cd3fbcab6f702889a9cd344ef95a3887c
VishalChoudhary4576 <vishalpoonia2005@gmail.com>
2026-04-28 15:53:29 +0530

    Add files via upload

---
+++ ExternalPractical/que1.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    
+    void push(int x) {
+        Node* newNode = new Node();
+        newNode->data = x;
+        newNode->next = top;
+        top = newNode;
+        cout << x << " pushed into stack\n";
+    }
+
+    
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack Underflow\n";
+            return;
+        }
+        Node* temp = top;
+        cout << temp->data << " popped from stack\n";
+        top = top->next;
+        delete temp;
+    }
+
+    
+    int peek() {
+        if (top == NULL) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
+        return top->data;
+    }
+
+    
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    
+    void display() {
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    cout << "Top element: " << s.peek() << endl;
+
+    s.pop();
+    s.display();
+
+    return 0;
+}
